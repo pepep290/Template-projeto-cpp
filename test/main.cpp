0dfe437cc263bd3becf195132847320402ddbda7
Pedro_Vitor <vitorpedro2904@gmail.com>
2026-05-06 18:08:51 -0300

    Mudando pequenas coisas nos testes

--- test/main.cpp
+++ test/main.cpp
@@ -3,14 +3,14 @@
 #include "bib.hpp"
 
 int main() {
-    // Testes da funcionalidade anterior (Validação)
+    // Testes da funcionalidade Validação
     assert(verificarDadoValido(20) == true);
     
-    // NOVOS Testes: Acerto Crítico
+    // Testes: Acerto Crítico
     assert(ehCritico(20, 20) == true);  // Tirou 20 num d20 -> Crítico!
     assert(ehCritico(5, 20) == false);  // Tirou 5 num d20 -> Normal.
     assert(ehCritico(6, 6) == true);    // Tirou 6 num d6 -> Crítico!
 
-    std::cout << ">>> TESTES REGRESSIVOS (VALIDACAO + CRITICO): PASSOU! <<<" << std::endl;
+    std::cout << "TESTES REGRESSIVOS (VALIDACAO + CRITICO): PASSOU" << std::endl;
     return 0;
 }
